f3d1d25fbb10730e3328dda973d95309d0f928b7
Justin Beemer <jubeemer@umich.edu>
2018-10-26 11:39:53 -0400

    Op lookup table

--- rpn.py
+++ rpn.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+import operator
+
+op = {
+     '+': operator.add,
+     '-': operator.sub,
+     '*': operator.mul,
+     '/': operator.floordiv,
+}
 
 def calculate(arg):
     # stack for calculator
@@ -15,15 +23,8 @@ def calculate(arg):
         except ValueError:
             val2 = stack.pop()
             val1 = stack.pop()
-            if token == '+':
-                result = val1 + val2
-            elif token == '-':
-                result = val1 - val2
-            elif token == '*':
-                result = val1 * val2
-            elif token == '/':
-                result = val1 / val2
-
+            func = op[token]
+            result = func(val1, val2)
             stack.append(result)
             return stack[0]
 
