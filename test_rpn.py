a60b41f67621b4f4478d13f9076975a22e489dc3
Justin Beemer <jubeemer@umich.edu>
2018-10-31 20:38:19 -0400

    added test for ^

--- test_rpn.py
+++ test_rpn.py
@@ -18,6 +18,10 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("4 2 /")
         self.assertEqual(2, result)
 
+    def test_exp(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
+
     def test_chain(self):
         result = rpn.calculate("1 1 + 2 *")
         self.assertEqual(4, result)
